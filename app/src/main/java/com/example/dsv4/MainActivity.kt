a2bcfd6e5679b3d4f4906ce02ea60a40f5b94d33
vasiliikarpenko <invweb@gmail.com>
2026-04-30 22:27:33 +0300

    in progress - navigation added

--- app/src/main/java/com/example/dsv4/MainActivity.kt
+++ app/src/main/java/com/example/dsv4/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.dsv4.ui.theme.DSV4Theme
 import com.example.dsv4.presentation.ui.CounterScreen
 import com.example.dsv4.presentation.ui.CounterViewModel
@@ -15,12 +18,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DSV4Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
-                    val viewModel: CounterViewModel by viewModels()
-                    CounterScreen(viewModel = viewModel)
+            val navController = rememberNavController()
+
+            NavHost(
+                startDestination = "CounterScreen",
+                navController = navController
+            ) {
+                composable("CounterScreen") {
+                    DSV4Theme {
+                        Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                            val viewModel: CounterViewModel by viewModels()
+                            CounterScreen(viewModel = viewModel)
+                        }
+                    }
                 }
             }
+
+
         }
     }
 }
